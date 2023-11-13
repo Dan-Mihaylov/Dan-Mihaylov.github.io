1248f86c836270082b75937d8b505e66e2baf4b8
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2023-11-13 00:45:13 +0000

    Upgraded the website
    Added functionality and changed personal card

--- scripts.js
+++ scripts.js
@@ -15,10 +15,35 @@ document.addEventListener("DOMContentLoaded", function () {
     closeNavButton.addEventListener("click", function () {
         openNavButton.style.opacity = 1;
         contentWrapper.classList.remove("blur");
-        navWindow.style.top = "-150%";
+        navWindow.style.top = "-100%";
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    let clickableDescriptions = document.querySelectorAll('.clickable-description');
+
+    clickableDescriptions.forEach(function (description) {
+        description.addEventListener('click', function () {
+            // Finding the parent element witht the class description-box
+            let parentDescriptionBox = this.closest('.description-box');
+
+            // Finding the <p> element inside the parent box
+            let content = parentDescriptionBox.querySelector('p');
+            let expand = parentDescriptionBox.querySelector('h4');
+
+            if (content.style.display === 'none' || window.getComputedStyle(content).display === 'none') {
+                content.style.display = 'block';
+                expand.style.display = 'none';
+            } else {
+                content.style.display = 'none';
+                expand.style.display = 'block';
+            }
+        });
+    });
+});
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     const openNavButton = document.getElementById("openNavButton");
